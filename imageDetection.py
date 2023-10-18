8cfde76921aea260e8e6de77174f1767cfa417a4
Cameron Henning <cwh35@pitt.edu>
2023-10-18 13:28:32 -0400

    4 - Removed other descriptors - used SIFT w/ FLANN matcher

--- imageDetection.py
+++ imageDetection.py
@@ -78,43 +78,58 @@ else:
 #         music_sheet_corners = np.argwhere(dst > 0.01 *dst.max())
 #         music_sheet_features.append(music_sheet_corners)
 
-# feature mtaching w/ ORB descriptor
-# img1 = cv.imread('templates/halfnote_lowf.PNG',cv.IMREAD_GRAYSCALE) # queryImage
-# img2 = cv.imread('sheets/samplesheet.PNG',cv.IMREAD_GRAYSCALE) # trainImage
-# # Initiate ORB detector
-# orb = cv.ORB_create()
-# # find the keypoints and descriptors with ORB
-# kp1, des1 = orb.detectAndCompute(img1,None)
-# kp2, des2 = orb.detectAndCompute(img2,None)
 
-# # create BFMatcher object
-# bf = cv.BFMatcher(cv.NORM_HAMMING, crossCheck=True)
-# # Match descriptors.
-# matches = bf.match(des1,des2)
-# # Sort them in the order of their distance.
-# matches = sorted(matches, key = lambda x:x.distance)
-# # Draw first 10 matches.
-# img3 = cv.drawMatches(img1,kp1,img2,kp2,matches[:10],None,flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
-# plt.imshow(img3),plt.show()
-
-# feature matching w/ SIFT descriptor
-img2 = cv.imread('templates/halfnote_lowf.png', cv.IMREAD_GRAYSCALE).astype('uint8')
-img2 = cv.resize(img2, (400,400))
-img1 = cv.imread('sheets/samplesheet.png', cv.IMREAD_GRAYSCALE).astype('uint8')
-sift = cv.SIFT_create()
-
-kp1, des1 = sift.detectAndCompute(img1, None)
-kp2, des2 = sift.detectAndCompute(img2, None)
-
-bf = cv.BFMatcher()
-matches = bf.knnMatch(des1, des2, k=2)
+# SIFT w/ FLANN based matcher
+templateDirectory = "templates"
+sheetDirectory = "sheets"
+outputDirectory = "results/siftWithFLANNResults"
 
 good = []
-for m,n in matches:
-    if m.distance < 0.75*n.distance:
-        good.append([m])
+# Loop through the music sheets
+for sheetFilename in os.listdir(sheetDirectory):
+    f = os.path.join(sheetDirectory, sheetFilename)
+    musicSheet = cv.imread(f ,cv.IMREAD_GRAYSCALE) # trainImage
 
-img3 = cv.drawMatchesKnn(img1, kp1, img2, kp2, good, None, flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
-plt.imshow(img3),plt.show()
+    # Loop through the templates
+    for templateFilename in os.listdir(templateDirectory):
+        file = os.path.join(templateDirectory, templateFilename) 
+        template = cv.imread(file ,cv.IMREAD_GRAYSCALE) # queryImage
 
+         # Cross-correlation between templates and music sheet
+        res = cv.matchTemplate(musicSheet, template, cv.TM_CCOEFF_NORMED)
+        # Get the min and max correlation value as well as locations of the matched points
+        min_val, max_val, min_loc, max_loc = cv.minMaxLoc(res)
 
+        # threshold to filter valid matches
+        threshold = 0.93
+        if max_val > threshold:
+            # SIFT with FLANN matcher
+            sift = cv.SIFT_create()
+            kp1, des1 = sift.detectAndCompute(template, None)
+            kp2, des2 = sift.detectAndCompute(musicSheet, None)
+
+            # KD-Tree (nearest neighbor searches)
+            FLANN_INDEX_KDTREE = 1
+            index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=7)
+            search_params = dict(checks=75) # checks = # of times to check consistency
+            # match the key points
+            flann = cv.FlannBasedMatcher(index_params, search_params)
+            matches = flann.knnMatch(des1, des2, k=2)
+
+            matchesMask = [[0, 0] for i in range(len(matches))]
+            for i, (m, n) in enumerate(matches):
+                if m.distance < 0.7 * n.distance:
+                    matchesMask[i] = [1, 0]
+
+            draw_params = dict(matchColor=(0, 255, 0),
+                                singlePointColor=(255, 0, 0),
+                                matchesMask=matchesMask,
+                                flags=cv.DrawMatchesFlags_DEFAULT)
+
+            matchingResult = cv.drawMatchesKnn(template, kp1, musicSheet, kp2, matches, None, **draw_params)
+
+            # Save the plot as an image
+            outputFilename = f"{sheetFilename}_{templateFilename}"
+            outputPath = os.path.join(outputDirectory, outputFilename)
+            cv.imwrite(outputPath, matchingResult)
+   
