4df6c4ec592f25f359638c522b19c2a23d6e8836
Cameron Henning <cwh35@pitt.edu>
2023-10-29 22:07:21 -0400

    5 - Narrowed matches to a region of interest, started encoding the matches

--- imageDetection.py
+++ imageDetection.py
@@ -23,113 +23,108 @@ else:
     print("Error: No image detected")
 """
 
-# directory = "templates"
-# template_features = []
-
-# # Harris Corner Detection on templates
-# for filename in os.listdir(directory):
-#     f = os.path.join(directory, filename)
-#     if os.path.isfile(f):
-#         # Read in image
-#         img = cv.imread(f)
-#         # Convert to gray scale
-#         gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-
-#         gray = np.float32(gray)
-#         dst = cv.cornerHarris(gray, 2, 3, 0.04)
-
-#         # Dilated for marking corners
-#         dst = cv.dilate(dst, None)
-
-#         # Threshold for optimum value
-#         img[dst > 0.01*dst.max()] = [0, 0, 255]
-
-#         # Store result in a folder for visual representation
-#         cv.imwrite(f"templateCorners/{filename}.png", img)
-
-#         # Extract corner features (coordinates) and store them in a list
-#         template_corners = np.argwhere(dst > 0.01 *dst.max())
-#         template_features.append(template_corners)
-
-# sheetDirectory = "sheets"
-# music_sheet_features = []
-
-# # Harris Corner Detection on the unlabeled music sheets
-# for sheetFilename in os.listdir(sheetDirectory):
-#     f = os.path.join(sheetDirectory, sheetFilename)
-#     if os.path.isfile(f):
-#          # Read in image
-#         img = cv.imread(f)
-#         # Convert to gray scale
-#         gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-
-#         gray = np.float32(gray)
-#         dst = cv.cornerHarris(gray, 2, 3, 0.04)
-
-#         # Dilated for marking corners
-#         dst = cv.dilate(dst, None)
-
-#         # Threshold for optimum value
-#         img[dst > 0.01*dst.max()] = [0, 0, 255]
-
-#         cv.imwrite(f"sheetCorners/{sheetFilename}.png", img)
-
-#         # Extract corner features (coordinates) and store them in a list
-#         music_sheet_corners = np.argwhere(dst > 0.01 *dst.max())
-#         music_sheet_features.append(music_sheet_corners)
-
-
 # SIFT w/ FLANN based matcher
 templateDirectory = "templates"
 sheetDirectory = "sheets"
 outputDirectory = "results/siftWithFLANNResults"
 
+# Mapping of template filenames to hexadecimal values
+template_to_hex = {
+    "24_timesignature.PNG": 0x24,
+    "34_timesignature.PNG": 0x34,
+    "44_timesignature.PNG": 0x44,
+    "40_tempo.PNG": 0x28,
+    "50_tempo.PNG": 0x32,
+    "60_tempo.PNG": 0x3C,
+    "70_tempo.PNG": 0x46,
+    "80_tempo.PNG": 0x50,
+    "90_tempo.PNG": 0x5A,
+    "100_tempo.PNG": 0x64,
+    "110_tempo.PNG": 0x6E,
+    "120_tempo.PNG": 0x78,
+    "130_tempo.PNG": 0x82,
+    "140_tempo.PNG": 0x8C,
+    "150_tempo.PNG": 0x96,
+    "160_tempo.PNG": 0xA0,
+    "170_tempo.PNG": 0xAA,
+    "180_tempo.PNG": 0xB4,
+    "190_tempo.PNG": 0xBE,
+    "200_tempo.PNG": 0xC8
+}
+
+hex_values = []  # Array to store hexadecimal values
+
+# Store the good matches here
 good = []
 # Loop through the music sheets
 for sheetFilename in os.listdir(sheetDirectory):
     f = os.path.join(sheetDirectory, sheetFilename)
-    musicSheet = cv.imread(f ,cv.IMREAD_GRAYSCALE) # trainImage
+    musicSheet = cv.imread(f ,cv.IMREAD_GRAYSCALE)  # trainImage
 
     # Loop through the templates
     for templateFilename in os.listdir(templateDirectory):
-        file = os.path.join(templateDirectory, templateFilename) 
-        template = cv.imread(file ,cv.IMREAD_GRAYSCALE) # queryImage
+        file = os.path.join(templateDirectory, templateFilename)
+        template = cv.imread(file ,cv.IMREAD_GRAYSCALE)  # queryImage
 
-         # Cross-correlation between templates and music sheet
+        # Cross-correlation between templates and music sheet
         res = cv.matchTemplate(musicSheet, template, cv.TM_CCOEFF_NORMED)
         # Get the min and max correlation value as well as locations of the matched points
         min_val, max_val, min_loc, max_loc = cv.minMaxLoc(res)
 
         # threshold to filter valid matches
-        threshold = 0.93
+        threshold = 0.8
         if max_val > threshold:
-            # SIFT with FLANN matcher
+            # Define the region of interest (ROI) around the max correlation location
+            h, w = template.shape
+            top_left = max_loc  # This is the top left point of the match
+            bottom_right = (top_left[0] + w, top_left[1] + h)
+            roi = musicSheet[top_left[1]:bottom_right[1], top_left[0]:bottom_right[0]]
+
+             # SIFT with FLANN matcher on ROI
             sift = cv.SIFT_create()
             kp1, des1 = sift.detectAndCompute(template, None)
-            kp2, des2 = sift.detectAndCompute(musicSheet, None)
+            kp2, des2 = sift.detectAndCompute(roi, None)
 
-            # KD-Tree (nearest neighbor searches)
-            FLANN_INDEX_KDTREE = 1
-            index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=7)
-            search_params = dict(checks=75) # checks = # of times to check consistency
-            # match the key points
-            flann = cv.FlannBasedMatcher(index_params, search_params)
-            matches = flann.knnMatch(des1, des2, k=2)
+            # Check the number of keypoints and adjust k accordingly
+            min_keypoints = min(len(kp1), len(kp2))
+            k_value = min(2, min_keypoints)  # Ensure k is at most 2, but not more than the number of keypoints
 
-            matchesMask = [[0, 0] for i in range(len(matches))]
-            for i, (m, n) in enumerate(matches):
-                if m.distance < 0.7 * n.distance:
-                    matchesMask[i] = [1, 0]
+            if k_value < 2:
+                print(f"Warning: Only {min_keypoints} keypoints found. Adjusting k to {k_value}")
 
-            draw_params = dict(matchColor=(0, 255, 0),
+            if k_value > 0:
+                # KD-Tree (nearest neighbor searches)
+                FLANN_INDEX_KDTREE = 1
+                index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=7)
+                search_params = dict(checks=75)  # checks = # of times to check consistency
+                # match the key points
+                flann = cv.FlannBasedMatcher(index_params, search_params)
+                matches = flann.knnMatch(des1, des2, k=k_value)  # Use adjusted k_value
+
+                matchesMask = [[0, 0] for i in range(len(matches))]
+                if k_value == 2:
+                    for i, (m, n) in enumerate(matches):
+                        if m.distance < 0.7 * n.distance:
+                            matchesMask[i] = [1, 0]
+                else:
+                    for i, m in enumerate(matches):
+                        matchesMask[i] = [1, 0]  # If k_value is 1, keep all matches
+
+                draw_params = dict(matchColor=(0, 255, 0),
                                 singlePointColor=(255, 0, 0),
                                 matchesMask=matchesMask,
                                 flags=cv.DrawMatchesFlags_DEFAULT)
+                
+                matchingResult = cv.drawMatchesKnn(template, kp1, roi, kp2, matches, None, **draw_params)
 
-            matchingResult = cv.drawMatchesKnn(template, kp1, musicSheet, kp2, matches, None, **draw_params)
+                # Save the plot as an image
+                outputFilename = f"{sheetFilename}_{templateFilename}"
+                outputPath = os.path.join(outputDirectory, outputFilename)
+                cv.imwrite(outputPath, matchingResult)
 
-            # Save the plot as an image
-            outputFilename = f"{sheetFilename}_{templateFilename}"
-            outputPath = os.path.join(outputDirectory, outputFilename)
-            cv.imwrite(outputPath, matchingResult)
-   
+                # Store the hexadecimal value corresponding to the matched template
+                hex_value = template_to_hex.get(templateFilename)
+                if hex_value is not None:
+                    hex_values.append(hex_value)
+    
+print(hex_values)
